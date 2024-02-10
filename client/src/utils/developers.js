556e3ddde41fb449787ca2e1d5f3859eb5248db3
praveennarni <praveenbabunarni1999@gmail.com>
2024-02-10 15:53:24 -0600

    animated text to landing page

---
+++ client/src/utils/developers.js
@@ -0,0 +1,32 @@
+export const developers = [
+    {
+        name: "Sandhya Paladugu",
+        img: '/img1.jpeg',
+        about: "Expertise in Citrix VDI administration. 1.6 years of experience as a System Admin at DXC."
+    },
+    {
+        name: "Manoj Vamanaguntla",
+        img: '/img2.jpeg',
+        about: "Expertise in Database and, Quality testing. 4 years of experience as Data Anayst"
+    },
+    {
+        name: "Aparna Marepally",
+        img: '/img4.jpeg',
+        about: "Passionate to excel in the field of full stack developer, Startups and entrepreneurship"
+    },
+    {
+        name: "Praveen Babu",
+        img: '/img3.jpeg',
+        about: "Achieved AWS (Amazon Web Services) and Python Certifications"
+    },
+    {
+        name: "Pooja Sri Ramineni",
+        img: '/img5.jpeg',
+        about: "Salesforce tester, expertise in error reporting and prioritizing them in JIRA"
+    },
+    {
+        name: "Vijaya Gopal Raju",
+        img: '/img6.jpeg',
+        about: "Good Skills in Salesforce. Good at team participation and time management"
+    },
+];
